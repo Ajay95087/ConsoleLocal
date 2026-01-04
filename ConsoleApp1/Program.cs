61859330e448b619eec74135715590c08fa345ea
Ajay95087 <kajay777006@gmail.com>
2026-01-04 12:35:53 +0530

    04-01-2025 variables

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,58 +1,54 @@
-﻿            using System;
-            using System.Collections.Generic;
-            using System.Linq;
-            using System.Text;
-            using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace consoleapp
+{
+    internal class Program
     {
-        internal class Program
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
+            /*Console.WriteLine("For loop");
+            for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine("For loop");
-                for (int i = 0; i < 10; i++)
-                {
-                    Console.WriteLine(i.ToString());
-                }
-                Console.WriteLine("while loop");
-                int a = 10;
-                while (a > 0)
-                {
-                    Console.WriteLine(a.ToString());
-                    a--;
-                }
-                Console.ReadLine();
-
-
-
-                //variables
-
-                /* string name = "Ajay";
-                int age = 25;
-                float height = 5.11f;
-
-                Console.WriteLine("Name:" + name);
-                Console.WriteLine("Age" + age);
-                Console.WriteLine("Height" + height);
-                Console.ReadLine(); */
-
-
-
-                //Operator
-                /*int x = 20;
-                {
-                    Console.WriteLine(x > 3 && x < 30);
-                }
-                Console.ReadLine();*/
-
-
-
-
+                Console.WriteLine(i.ToString());
             }
-        }
-    }
+            Console.WriteLine("while loop");
+            int a = 10;
+            while (a > 0)
+            {
+                Console.WriteLine(a.ToString());
+                a--;
+            }
+            Console.ReadLine();*/
 
-}
+
+
+            //variables
+
+             string name = "Ajay";
+            int age = 25;
+            float height = 5.11f;
+
+            Console.WriteLine("Name:" + name);
+            Console.WriteLine("Age" + age);
+            Console.WriteLine("Height" + height);
+            Console.ReadLine(); 
+
+
+
+            //Operator
+            /*int x = 20;
+            {
+                Console.WriteLine(x > 3 && x < 30);
+            }
+            Console.ReadLine();*/
+
+
+
+
+        }
     }
 }
