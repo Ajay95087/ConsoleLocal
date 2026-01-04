2f0cbc10ecbcce6f217387ed1fdf66b66aba971e
Ajay95087 <kajay777006@gmail.com>
2026-01-04 21:02:26 +0530

    04-01-2025 commit all

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,7 +9,9 @@ namespace consoleapp
     internal class Program
     {
         static void Main(string[] args)
+
         {
+
             /*Console.WriteLine("For loop");
             for (int i = 0; i < 10; i++)
             {
@@ -39,11 +41,15 @@ namespace consoleapp
 
 
             //Operator
+
             /*int x = 20;
             {
                 Console.WriteLine(x > 3 && x < 30);
             }
             Console.ReadLine();*/
+
+
+
 
 
             //String
@@ -55,13 +61,16 @@ namespace consoleapp
             Console.ReadLine();*/
 
 
-            string data = "apple, banana, mango";
+
+            /*string data = "apple, banana, mango";
 
             //data.ToUpper();
             //data.ToLower();
 
 
             string[] fruits = data.Split('-');
+
+            // foreach
 
             foreach (var item in fruits) 
             {
@@ -70,6 +79,104 @@ namespace consoleapp
 
             Console.WriteLine(data.ToUpper());
             //Console.WriteLine(data.ToLower());
+
+            Console.ReadLine();*/
+
+
+
+            //Math
+
+            /* int a = 5;
+
+             Console.WriteLine(Math.Abs(a));
+             Console.WriteLine(Math.Pow(3, 2));
+             Console.WriteLine(Math.Sqrt(16));
+             Console.WriteLine(Math.Max(10, 5));
+
+             Console.ReadLine();*/
+
+
+
+
+            //break    
+
+
+
+            /*for (int i = 1; i <= 5; i++) 
+            {
+                if (i == 3)
+                    break;
+
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();*/
+
+
+
+            //continue
+
+
+
+
+            /*for (int i = 1; i <= 10; i++)
+            {
+                if (i == 3)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            };
+
+            Console.ReadLine();*/
+
+
+
+
+            //Array + Loop
+
+
+
+            /*string a = "";
+            do
+            {
+                Console.WriteLine("Press 1 for add");
+                Console.WriteLine("Press 2 for sub");
+                Console.WriteLine("Press 0 for exit");
+                a=Console.ReadLine();
+                if (a == "1")
+                {
+                    Console.WriteLine("Enter First no");
+                    int firstno =Convert.ToInt32( Console.ReadLine());
+                    Console.WriteLine("Enter second no");
+                    int secondno = Convert.ToInt32(Console.ReadLine());
+                    int result = firstno + secondno;
+                    Console.WriteLine("Result is "+result);
+                }
+            } while (a != "0");*/
+
+
+
+
+            //Array
+
+
+
+
+            string fullname= "Ajay kumar singh";
+
+
+            String[] part = fullname.Split(' ');
+
+            String firstname = part[0];
+            String middlename = part[1];
+            String lastname = part[2];
+
+            Console.WriteLine("Your first name is;"+ firstname);
+            Console.WriteLine("Your middle name is;" + middlename);
+            Console.WriteLine("Your last name is;" + lastname );
+
+
 
             Console.ReadLine();
 
